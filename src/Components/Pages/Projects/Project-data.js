024882e956a81b6179a0d412a946f49a5fb5618c
Ranjan1501 <ranjankumar1757@gmail.com>
2022-04-23 00:23:14 +0530

    removed extra code

--- src/Components/Pages/Projects/Project-data.js
+++ src/Components/Pages/Projects/Project-data.js
@@ -1,24 +1,8 @@
-// import HTML5 from "../../assets/images/TechPic/html.svg";
-// import CSS3 from "../../assets/images/TechPic/css.svg";
-// import REACT from "../../assets/images/TechPic/react.svg";
-// import REDUX from "../../assets/images/TechPic/redux.svg";
-// import EJS from "../../assets/images/TechPic/ejs-svgrepo-com.svg";
 
-// import NODE_JS from "../../assets/images/TechPic/nodejs.svg";
-// import MONGODB from "../../assets/images/TechPic/mongoDB.svg";
-// import MSSQL from "../../assets/images/TechPic/mysql.svg";
-// import GIT from "../../assets/images/TechPic/git.svg";
-// import GITHUB_PAGES from "../../assets/images/TechPic/github.svg";
-// import JAVASCRIPT from "../../assets/images/TechPic/js.svg";
 import ZoomCar from "../../assets/images/TechPic/zoomcar.png";
 import Shein from "../../assets/images/TechPic/Shein.png";
 import BookMyShow from "../../assets/images/TechPic/bookMyShow.jpg";
 
-// import shoes4us from "../../assets/images/TechPic/shoes4us.png";
-// import fatsecret from "../../assets/images/TechPic/fatsecret.png";
-// import gdg from "../../assets/images/TechPic/gdg.png";
-// import onlineEditor from "../../assets/images/TechPic/online-editor.png";
-// import starWars from "../../assets/images/TechPic/star-wars.png";
 
 export const projects = [
   {
@@ -66,44 +50,5 @@ export const projects = [
     //  technologies: [HTML5, CSS3, JAVASCRIPT, REACT],
   },
 
-  //   {
-  //     project_name: "Online Editor",
-  //     project_image: onlineEditor,
-  //     Project_description:
-  //       "Online Editor for HTML, CSS, Javascript code. Which will run live. Code is also persist when we refresh the page.",
-  //     github_link: "https://github.com/rohanranjan25061996/online-editor",
-  //     deployed_link: "https://rohan-online-html-css-js-editor.netlify.app/",
-  //     technologies: [HTML5, CSS3, JAVASCRIPT, REACT],
-  //   },
-  //   {
-  //     project_name: "STARS WAR API Search",
-  //     project_image: starWars,
-  //     Project_description:
-  //       "An Optimize Search Data. Using STAR-WARS API creating Live Search Box and Character page with some cool details about characters.",
-  //     github_link:
-  //       "https://github.com/rohanranjan25061996/urban_pipper_assingment",
-  //     deployed_link: "https://rohan-ranjan-urban-piper-assignments.netlify.app/",
-  //     technologies: [HTML5, CSS3, JAVASCRIPT, REACT],
-  //   },
-  //   {
-  //     project_name: "FatSecret Clone",
-  //     project_image: fatsecret,
-  //     Project_description:
-  //       "FatSecret is a mobile application, website and API that lets users track their nutrition, exercise and weight. Cloned it under 3 days with a team of 3 people. Technologies used here are HTML, CSS, JavaScript",
-  //     github_link: "https://github.com/rohanranjan25061996/fatsecret_clone",
-  //     deployed_link:
-  //       "https://drive.google.com/file/d/15eIuE5mAkoB2DrXlgGZ78kgDq-2leuXF/view",
-  //     technologies: [HTML5, CSS3, JAVASCRIPT],
-  //   },
-  //   {
-  //     project_name: "Google Digital Garage Clone",
-  //     project_image: gdg,
-  //     Project_description:
-  //       "The Digital Garage is a non-profit nationwide programme from Google delivering free digital skills training via an online learning platform. Cloned it under 4 days with a team of 3 people. Technologies used here are HTML, CSS",
-  //     github_link:
-  //       "https://github.com/rohanranjan25061996/google-digital-garage-clone-Unit-1-",
-  //     deployed_link:
-  //       "https://drive.google.com/file/d/1tdtbRG_HopE-PHJDupackR1J43mlapCz/view?usp=sharing",
-  //     technologies: [HTML5, CSS3],
-  //   },
+
 ];
