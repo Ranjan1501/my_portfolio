6c7af233fe9b5920d561fb8258d1ebb9a15cc191
Ranjan1501 <ranjankumar1757@gmail.com>
2023-04-03 10:47:56 +0530

    Update: add few skills

--- src/Components/Pages/Skills/skill-data.js
+++ src/Components/Pages/Skills/skill-data.js
@@ -4,31 +4,50 @@ import L_REACT from "../../assets/images/TechPic/react.svg";
 import L_REDUX from "../../assets/images/TechPic/redux.svg";
 import L_NODE_JS from "../../assets/images/TechPic/nodejs.svg";
 import L_MONGODB from "../../assets/images/TechPic/mongoDB.svg";
-import L_GIT from "../../assets/images/TechPic/git.svg";
+// import L_GIT from "../../assets/images/TechPic/git.svg";
 import L_JAVASCRIPT from "../../assets/images/TechPic/js.svg";
 import L_DSA from "../../assets/images/TechPic/DSA.svg";
 import L_EJS from "../../assets/images/TechPic/ejs-svgrepo-com.svg";
 import L_EXPRESS from "../../assets/images/TechPic/expressjs.svg";
+import typescript from "../../assets/images/TechPic/typescript.svg";
+import php from "../../assets/images/TechPic/php.svg";
+import postgresql from "../../assets/images/TechPic/postgresql.svg";
+import phpMyAdmin from "../../assets/images/TechPic/phpmyadmin.svg";
+import docker from "../../assets/images/TechPic/docker.svg";
+import postman from "../../assets/images/TechPic/postman.svg";
+import java from "../../assets/images/TechPic/java.svg";
 
 export const skills = {
   frontend: [
-    {
-      link: "https://en.wikipedia.org/wiki/HTML5",
-      imgAltText: "HTML 5",
-      imgSrc: L_HTML5,
-      skillName: "HTML5",
-    },
-    {
-      link: "https://medium.com/beginners-guide-to-mobile-web-development/whats-new-in-css-3-dcd7fa6122e1",
-      imgAltText: "CSS 3",
-      imgSrc: L_CSS3,
-      skillName: "CSS3",
-    },
     {
       link: "https://www.javascript.com/",
       imgAltText: "JavaScript",
       imgSrc: L_JAVASCRIPT,
       skillName: "JavaScript",
+    },
+    {
+      link: "https://nodejs.org/en/",
+      imgAltText: "Node.js",
+      imgSrc: L_NODE_JS,
+      skillName: "Node.js",
+    },
+    {
+      link: "https://www.typescriptlang.org/",
+      imgAltText: "typescript",
+      imgSrc: typescript,
+      skillName: "Typescript",
+    },
+    {
+      link: "https://www.php.net/",
+      imgAltText: "Php",
+      imgSrc: php,
+      skillName: "PHP",
+    },
+    {
+      link: "https://docs.oracle.com/en/java/",
+      imgAltText: "java",
+      imgSrc: java,
+      skillName: "Java",
     },
     {
       link: "https://reactjs.org/",
@@ -43,10 +62,16 @@ export const skills = {
       skillName: "Redux",
     },
     {
-      link: "https://nodejs.org/en/",
-      imgAltText: "Node.js",
-      imgSrc: L_NODE_JS,
-      skillName: "Node.js",
+      link: "https://expressjs.com/",
+      imgAltText: "Express",
+      imgSrc: L_EXPRESS,
+      skillName: "Express Js",
+    },
+    {
+      link: "https://www.postgresql.org/",
+      imgAltText: "postgresql",
+      imgSrc: postgresql,
+      skillName: "postgresql",
     },
     {
       link: "https://www.mongodb.com/",
@@ -55,24 +80,40 @@ export const skills = {
       skillName: "MongoDB",
     },
     {
-      link: "https://git-scm.com/",
-      imgAltText: "GIT",
-      imgSrc: L_GIT,
-      skillName: "GIT",
-    },
-
-    {
-      link: "https://expressjs.com/",
-      imgAltText: "Express",
-      imgSrc: L_EXPRESS,
-      skillName: "Express Js",
-    },
-
-    {
       link: "https://ejs.co/",
       imgAltText: "EJs",
       imgSrc: L_EJS,
       skillName: "EJS",
+    },
+    {
+      link: "https://www.phpmyadmin.net/",
+      imgAltText: "phpMyAdmin",
+      imgSrc: phpMyAdmin,
+      skillName: "phpMyAdmin",
+    },
+    {
+      link: "https://www.docker.com/",
+      imgAltText: "docker",
+      imgSrc: docker,
+      skillName: "Docker",
+    },
+    {
+      link: "https://www.postman.com/",
+      imgAltText: "postman",
+      imgSrc: postman,
+      skillName: "Postman",
+    },
+    {
+      link: "https://en.wikipedia.org/wiki/HTML5",
+      imgAltText: "HTML 5",
+      imgSrc: L_HTML5,
+      skillName: "HTML5",
+    },
+    {
+      link: "https://medium.com/beginners-guide-to-mobile-web-development/whats-new-in-css-3-dcd7fa6122e1",
+      imgAltText: "CSS 3",
+      imgSrc: L_CSS3,
+      skillName: "CSS3",
     },
   ],
 };
