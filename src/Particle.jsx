23e6f375cf9af0ff0ecea941737829ffd8cf8e1b
ranjan.thakur1 <ranjan.thakur@nielsen.com>
2026-01-16 14:53:53 +0530

    fixed build issue

--- src/Particle.jsx
+++ src/Particle.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Particles from "react-particles";
-import part from "./particle.json"
+import part from "./particle.json";
 
-function Demo(){
-
-
-   return(
-      <>
+function Demo() {
+  return (
+    <>
       <Particles id="tsparticles" options={part} height="700px" />
-      </>
-   )
+    </>
+  );
 }
 
-export {Demo}
+export { Demo };
